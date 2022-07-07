652efd5ef7d03c9f17b3b3b54dcce2a83fe12103
Mher Hovhannisyan <mh3r99@gmail.com>
2022-07-07 23:52:03 +0400

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/HeaderFooter/Header";
+import "./resources/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
