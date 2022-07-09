56870695342f0e54ed2c3e78e52914dd1181b037
Mher Hovhannisyan <mh3r99@gmail.com>
2022-07-09 22:33:51 +0400

    location and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Featured from "./components/Featured/Featured";
+import Footer from "./components/HeaderFooter/Footer";
 import Header from "./components/HeaderFooter/Header";
 import Highlights from "./components/Highlights/Highlights";
+import Location from "./components/Location/Location";
 import Pricing from "./components/Pricing/Pricing";
 import VenueNfo from "./components/VenueNfo/VenueNfo";
 import "./resources/styles.css";
@@ -13,6 +15,8 @@ function App() {
       <VenueNfo />
       <Highlights />
       <Pricing />
+      <Location />
+      <Footer/>
     </div>
   );
 }
